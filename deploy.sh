bc0b849d60bb8c6b5c94ae44095dcaf9034e79a9
Daniel DeGroff <daniel@inversoft.com>
2018-09-06 00:49:38 -0600

    Try to enforce running on fusionauth set set +s for user.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+HOSTNAME=`hostname`
+
+if [ ! "$HOSTNAME" = "fusionauth" ]; then
+  echo "You are only supposed to run this on fusionauth.io"
+  exit 0
+fi
+
 set -e
 
 cd /var/git/fusionauth-site
