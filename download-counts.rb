366c2a8f722a20d83eb457c73227a6f1a2b7da34
Brian Pontarelli <brian@inversoft.com>
2018-09-20 11:57:54 -0600

    Added whitelist check and remove whitespace from CSV output

--- download-counts.rb
+++ download-counts.rb
@@ -3,6 +3,14 @@ require 'fileutils'
 
 temp_dir = "/tmp/fusionauth-access-logs"
 output_file = "/var/www/collateral/download-count.csv"
+whitelisted_ip_patterns_file = "/etc/monit/whitelist_ips.regex"
+
+# Load the white regex
+whitelisted_ip_patterns = []
+File.readlines("#{whitelisted_ip_patterns_file}").each do |l|
+  whitelisted_ip_patterns << Regexp::new(l.strip)
+end
+
 
 # Clean up the existing access logs
 FileUtils.rm_rf(temp_dir)
@@ -16,7 +24,12 @@ Dir.foreach(temp_dir) do |file|
     date = file.gsub(/FusionAuthAccesssLog_usage_([0-9]{4})_([0-9]{2})_([0-9]{2}).*/, '\1\2\3')
     File.readlines("#{temp_dir}/#{file}").each do |l|
       if l =~ /fusionauth-app[-_0-9all.]*(deb|rpm|zip)"/
-        counts[date] = counts[date] + 1
+
+        # Check to see if the IP is whitelisted
+        ip = /"\d+?","(.+?)"/.match(l).captures[0]
+        if whitelisted_ip_patterns.find_index { |pattern| pattern.match(ip) } == nil
+          counts[date] = counts[date] + 1
+        end
       end
     end
   end
@@ -28,6 +41,6 @@ File.open(output_file, "w", :encoding => "UTF-8") do |f|
 
   # Write the counts
   counts.sort.each do |date,count|
-    f.puts("#{date}, #{count}\n")
+    f.puts("#{date},#{count}\n")
   end
 end
