10f5815820795602114c73e67a367a651ca6d93e
Daniel DeGroff <daniel@inversoft.com>
2018-10-04 23:40:30 -0600

    updates to the download counter

--- download-counts.rb
+++ download-counts.rb
@@ -25,10 +25,20 @@ Dir.foreach(temp_dir) do |file|
     File.readlines("#{temp_dir}/#{file}").each do |l|
       if l =~ /fusionauth-app[-_0-9all.]*(deb|rpm|zip)"/
 
+        if not counts.has_key? date
+          counts[date] = [0,0,0]
+        end
+
         # Check to see if the IP is whitelisted
         ip = /"\d+?","(.+?)"/.match(l).captures[0]
         if whitelisted_ip_patterns.find_index { |pattern| pattern.match(ip) } == nil
-          counts[date] = counts[date] + 1
+          if l =~ /deb/
+            counts[date][0] = counts[date][0] + 1
+          elsif l =~ /rpm/
+            counts[date][1] = counts[date][1] + 1
+          else
+            counts[date][2] = counts[date][2] + 1
+          end
         end
       end
     end
@@ -37,10 +47,14 @@ end
 
 File.open(output_file, "w", :encoding => "UTF-8") do |f|
   # Write the header
-  f.puts("Date,Download count\n")
+  f.puts("Date,Total,DEB,RPM,Zip\n")
 
   # Write the counts
   counts.sort.each do |date,count|
-    f.puts("#{date},#{count}\n")
+    total = count[0] + count[1] + count[2]
+    f.puts("#{date},#{total},#{count[0]},#{count[1]},#{count[2]}\n")
   end
+  # Add some color to the chart
+  f.puts("Color,#47B050,#FF7E0E,#820000,#FFFF00")
+  f.puts("Type,area,stackedarea,stackedarea,stackedarea")
 end
