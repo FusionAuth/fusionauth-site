d88297d7b121981b00f6e172c301997f73d40858
Tyler Scott <tyler@inversoft.com>
2018-11-14 09:25:38 -0700

    Attempting to fix dirty builds and updating defaults
    https://www.pivotaltracker.com/story/show/161942989

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Note: if you make changes to this file, you will have to run sb push twice because the first execution will run the old version.
+
 if [ ! "$(hostname)" = "fusionauth" ]; then
   echo "You are only supposed to run this on fusionauth.io, run sb push instead."
   exit 0
@@ -10,6 +12,9 @@ set -e
 cd /var/git/fusionauth-site
 
 git pull
+
+# Prevent dirty builds
+bundle exec jekyll clean
 
 bundle exec jekyll build
 
