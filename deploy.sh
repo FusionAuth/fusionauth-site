bf4516d9391792f69dc998c978a6ebc55dbdae6a
Tyler Scott <tyler@inversoft.com>
2019-01-18 15:55:23 -0700

    Make the site self update bundle

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Note: if you make changes to this file, you will have to run sb push twice because the first execution will run the old version.
 
-if [ ! "$(hostname)" = "fusionauth" ]; then
+if [[ ! "$(hostname)" = "fusionauth" ]]; then
   echo "You are only supposed to run this on fusionauth.io, run sb push instead."
   exit 0
 fi
@@ -12,6 +12,9 @@ set -e
 cd /var/git/fusionauth-site
 
 git pull
+
+# Update dependencies
+bundle update
 
 # Prevent dirty builds
 bundle exec jekyll clean
