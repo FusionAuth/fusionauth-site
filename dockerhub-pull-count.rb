5b9a3233e238f3f37631812a4ab9844536712d5e
Daniel DeGroff <daniel@inversoft.com>
2019-03-15 16:26:52 -0600

    Update dockerhub pull count script

--- dockerhub-pull-count.rb
+++ dockerhub-pull-count.rb
@@ -2,56 +2,28 @@
 require 'fileutils'
 require 'json'
 require 'net/http'
+require 'csv'
 
-data_file = "/var/www/collateral/dockerhub-pull-count.json"
+data_file = "/var/www/collateral/dockerhub-pull-count-raw.csv"
 output_file = "/var/www/collateral/dockerhub-pull-count.csv"
 url = 'https://hub.docker.com/v2/repositories/fusionauth/fusionauth-app/'
 response = JSON.parse(Net::HTTP.get(URI(url)))
-data_json = JSON.parse(File.read data_file)
 
-today = Time.now.strftime("%Y%m%d")
-yesterday = (Time.now - (3600 * 24)).strftime("%Y%m%d")
 total_count = response['pull_count']
 star_count = response['star_count']
 
-# Update totals
-data_json['total']['count'] = total_count
-data_json['total']['stars'] = star_count
-
-# Create an entry for yesterday if it doesn't exist
-if data_json['daily'][yesterday] == nil
-  data_json['daily'][yesterday] = {
-      count: 0,
-      stars: 0,
-      total: {
-          count: total_count,
-          stars: star_count
-      }
-  }
+# Write out a new line to the raw file
+File.open(data_file, "a", :encoding => "UTF-8") do |f|
+  f.puts "#{Time.now.strftime("%Y%m%d %H%M%S")},#{total_count},#{star_count}"
 end
 
-# Create an entry for today if it doesn't exist
-if data_json['daily'][today] == nil
-  data_json['daily'][today] = {
-      count: 0,
-      stars: 0,
-      total: {
-          count: total_count,
-          stars: star_count
-      }
-  }
-end
+totals = {}
+stars = {}
 
-# Calculate the difference from yesterday for pulls and stars
-today_count = total_count.to_i - data_json['daily'][yesterday]['total']['count'].to_i
-today_star = star_count.to_i - data_json['daily'][yesterday]['total']['stars'].to_i
-
-data_json['daily'][today]['count'] = today_count
-data_json['daily'][today]['stars'] = today_star
-
-# Save the JSON file and then write out new CSV
-File.open(data_file, "w", :encoding => "UTF-8") do |f|
-  f.write(JSON.pretty_generate(data_json))
+CSV.foreach(data_file) do |row|
+  date = row[0][0, 8]
+  totals[date] = row[1]
+  stars[date] = row[2]
 end
 
 File.open(output_file, "w", :encoding => "UTF-8") do |f|
@@ -60,12 +32,12 @@ File.open(output_file, "w", :encoding => "UTF-8") do |f|
   f.puts("Date,Pulls,Stars\n")
 
   # Write each day sorted
-  data_json['daily'].sort.to_h.each do |day, value|
-    f.puts("#{day},#{value['count']},#{value['stars']}")
+  totals.sort.to_h.each do |date, total|
+    f.puts("#{date},#{total},#{stars[date]}")
   end
 
-  f.puts("Cumulative,0,0")
-  f.puts("Type,stackedarea,line")
+  f.puts("Color,#47B050,#FFFF00")
+  f.puts("Type,area,stackedarea,line")
   f.puts("Total,#{total_count},#{star_count}")
 
 end
