33a378ca88296b0e436188573b846afbf78a067b
Daniel DeGroff <daniel@fusionauth.io>
2020-06-17 16:43:24 -0600

    Fix my pebkac

--- src/deploy.sh
+++ src/deploy.sh
@@ -35,5 +35,4 @@ chown www-data:www-data /var/www/fusionauth.io
 
 # Build a sym link for the login style, assuming only one website-style file
 cd /var/www/fusionauth.io/assets/css
-rm fusionauth-login-style.css
 ln -s `ls fusionauth-website-style*.css|head -n 1` fusionauth-login-style.css
