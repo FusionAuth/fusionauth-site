5649d772de07b2e2e4caff61d545028cb63d38f3
Daniel DeGroff <daniel@fusionauth.io>
2022-07-04 23:24:15 -0600

    try some hooks

--- site/_plugins/algolia_hooks.rb
+++ site/_plugins/algolia_hooks.rb
@@ -8,12 +8,32 @@ module Jekyll
       end
 
       def self.before_indexing_each(record, _node, _context)
+        if _node != nil
+
+          # Simplify h1, h2, h3, h4, h5 and h6 indexing in case of embedded elements.
+          # - Specifically, the h1 in release notes includes an inner <a> that does not need to be indexed.
+          if %w[h1 h2 h3 h4 h5 h6].include? _node.name
+            record[:html] = _node.text
+            record[:text] = _node.text.strip
+          end
+
+          # Collapse JSON for better searching.
+          # - Sort of a variation of https://github.com/algolia/jekyll-algolia/issues/63#issuecomment-380863265
+          #   .content, .text, .to_str, and .inner_text are all aliases.
+          # - Do we have to also modify record[:html] ?
+          if _node.name == 'code' && _node.attr('data-lang') == 'json'
+            record[:html] = _node.text.strip
+            record[:text] = _node.text
+          end
+
+        end
+
         # skip really long records
         if record[:content] && record[:content].length > 9000
           puts "skipping some content on " + record[:url] + " as it has a length of " +record[:content].length.to_s
           return nil
         end
-        
+
         # from https://community.algolia.com/jekyll-algolia/hooks.html
         # don't send entire html
         # we don't need it, takes up more space
