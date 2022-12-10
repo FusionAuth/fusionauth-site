4b88893fcbe04d797f7e2af3be6be657d9fbd712
Brian Pontarelli <brian@fusionauth.io>
2022-12-10 14:01:41 -0700

    Revert back to ES6 without private fields or methods. They don't work with Uglifier.

--- site/assets/js/Pricing-0.3.0.js
+++ site/assets/js/Pricing-0.3.0.js
@@ -5,81 +5,61 @@
 
 // noinspection DuplicatedCode
 class FusionAuthPriceCalculator {
-  #communityButton;
-  #hosting;
-  #hostingPrice;
-  #hostingPriceDiv;
-  #hostingSelectionDiv;
-  #monthlyActiveUserSlider;
-  #monthlyActiveUserSliderMin;
-  #monthlyActiveUserSliderMax;
-  #monthlyActiveUserValue;
-  #plan;
-  #planPrice;
-  #planPriceDiv;
-  #planSelectionDiv;
-  #priceModel;
-  #purchaseButton;
-  #starterButton;
-  #step;
-  #sumDiv;
-  #url;
-
   constructor() {
-    this.#hostingSelectionDiv = document.getElementById('hosting-selection');
-    this.#hostingPriceDiv = document.getElementById('hosting-price');
-    this.#planSelectionDiv = document.getElementById('plan-selection');
-    this.#planPriceDiv = document.getElementById('plan-price');
-    this.#sumDiv = document.getElementById('sum');
-    this.#monthlyActiveUserSlider = document.querySelector('input[name=monthly-active-users]');
-    this.#monthlyActiveUserSlider.addEventListener('input', event => this.#handleSliderChange(event));
-    this.#monthlyActiveUserSlider.addEventListener('mouseup', event => this.#handleSliderChange(event));
-    this.#monthlyActiveUserSliderMin = parseInt(this.#monthlyActiveUserSlider.getAttribute('min'));
-    this.#monthlyActiveUserSliderMax = parseInt(this.#monthlyActiveUserSlider.getAttribute('max'));
-    this.#monthlyActiveUserValue = document.getElementById('monthly-active-users-value');
-    this.#communityButton = document.querySelector('a[data-plan=Community]');
-    this.#starterButton = document.querySelector('a[data-plan=Starter]');
-    this.#purchaseButton = document.getElementById('purchase-button');
+    this.hostingSelectionDiv = document.getElementById('hosting-selection');
+    this.hostingPriceDiv = document.getElementById('hosting-price');
+    this.planSelectionDiv = document.getElementById('plan-selection');
+    this.planPriceDiv = document.getElementById('plan-price');
+    this.sumDiv = document.getElementById('sum');
+    this.monthlyActiveUserSlider = document.querySelector('input[name=monthly-active-users]');
+    this.monthlyActiveUserSlider.addEventListener('input', event => this._handleSliderChange(event));
+    this.monthlyActiveUserSlider.addEventListener('mouseup', event => this._handleSliderChange(event));
+    this.monthlyActiveUserSliderMin = parseInt(this.monthlyActiveUserSlider.getAttribute('min'));
+    this.monthlyActiveUserSliderMax = parseInt(this.monthlyActiveUserSlider.getAttribute('max'));
+    this.monthlyActiveUserValue = document.getElementById('monthly-active-users-value');
+    this.communityButton = document.querySelector('a[data-plan=Community]');
+    this.starterButton = document.querySelector('a[data-plan=Starter]');
+    this.purchaseButton = document.getElementById('purchase-button');
 
-    this.#hostingPrice = 0;
-    this.#planPrice = 0;
-    this.#loadState();
+    this.hostingPrice = 0;
+    this.planPrice = 0;
+    this._loadState();
 
     document.querySelectorAll('a[data-step]')
-            .forEach(e => e.addEventListener('click', event => this.#handleStepClick(event)))
+            .forEach(e => e.addEventListener('click', event => this._handleStepClick(event)))
     document.querySelectorAll('a[data-plan]')
-            .forEach(e => e.addEventListener('click', event => this.#handlePlanClick(event)))
-    window.addEventListener('popstate', event => this.#handleStateChange(event));
+            .forEach(e => e.addEventListener('click', event => this._handlePlanClick(event)))
+    window.addEventListener('popstate', event => this._handleStateChange(event));
 
     fetch('https://account.fusionauth.io/ajax/purchase/price-model')
         .then(response => response.json())
         .then(json => {
-          this.#priceModel = json;
-          this.#changeStep();
+          this.priceModel = json;
+          this._changeStep();
         })
   }
 
-  #calculateHostingPrice(type) {
+  _calculateHostingPrice(type) {
     let price = 0;
 
     if (type === 'basic-cloud') {
-      price = this.#priceModel.ec2['medium'] / 2;
+      price = this.priceModel.ec2['medium'] / 2;
     } else if (type === 'business-cloud') {
-      price = this.#priceModel.ec2['medium'] + this.#priceModel.rds['medium'];
+      price = this.priceModel.ec2['medium'] + this.priceModel.rds['medium'];
     } else if (type === 'ha-cloud') {
-      price = (2 * this.#priceModel.ec2['medium']) + this.#priceModel.elb.base + (this.#priceModel.rds['medium'] * 2);
+      price = (2 * this.priceModel.ec2['medium']) + this.priceModel.elb.base + (this.priceModel.rds['medium'] * 2);
     }
 
     return price;
   }
 
-  #calculatePlanPrice(plan) {
+  _calculatePlanPrice(plan) {
     if (plan === 'Community') {
       return 0;
     }
 
-    var mau = parseInt(this.#monthlyActiveUserSlider.value);
-    var planPricing = this.#priceModel.plan.tierPricing[plan];
+    var mau = parseInt(this.monthlyActiveUserSlider.value);
+    var planPricing = this.priceModel.plan.tierPricing[plan];
     var increments = mau / 10000;
     var price;
     if (increments < 10) {
@@ -93,44 +73,44 @@ class FusionAuthPriceCalculator {
     return price;
   }
 
-  #changeStep() {
-    this.#redraw();
-    if (this.#step) {
-      this.#url.searchParams.set('step', this.#step);
+  _changeStep() {
+    this._redraw();
+    if (this.step) {
+      this.url.searchParams.set('step', this.step);
     }
-    if (this.#hosting) {
-      this.#url.searchParams.set('hosting', this.#hosting);
+    if (this.hosting) {
+      this.url.searchParams.set('hosting', this.hosting);
     }
-    if (this.#plan) {
-      this.#url.searchParams.set('plan', this.#plan);
+    if (this.plan) {
+      this.url.searchParams.set('plan', this.plan);
     }
   }
 
-  #handlePlanClick(event) {
+  _handlePlanClick(event) {
     // Let the download click bubble
     const plan = event.currentTarget.dataset.plan;
-    if (this.#hosting === 'self-hosting' && plan === 'Community') {
+    if (this.hosting === 'self-hosting' && plan === 'Community') {
       return;
     }
 
     event.stopPropagation();
     event.preventDefault();
     if (plan) {
-      this.#plan = plan;
+      this.plan = plan;
     }
-    this.#redraw();
+    this._redraw();
   }
 
-  #handleSliderChange() {
-    const mau = parseInt(this.#monthlyActiveUserSlider.value);
+  _handleSliderChange() {
+    const mau = parseInt(this.monthlyActiveUserSlider.value);
     let mauText = new Intl.NumberFormat('en').format(mau);
     if (mau === 1000000) {
       mauText += "+";
     }
-    this.#monthlyActiveUserValue.innerText = mauText;
+    this.monthlyActiveUserValue.innerText = mauText;
 
-    const width = this.#monthlyActiveUserSlider.offsetWidth;
-    const ratio = (mau - this.#monthlyActiveUserSliderMin) / (this.#monthlyActiveUserSliderMax - this.#monthlyActiveUserSliderMin);
+    const width = this.monthlyActiveUserSlider.offsetWidth;
+    const ratio = (mau - this.monthlyActiveUserSliderMin) / (this.monthlyActiveUserSliderMax - this.monthlyActiveUserSliderMin);
     let left = ((ratio * (width - 30)) + 5);
     if (left > (width / 2)) {
       left -= 10;
@@ -138,16 +118,16 @@ class FusionAuthPriceCalculator {
     if (left > (width - 50)) {
       left -= 15;
     }
-    this.#monthlyActiveUserValue.style.left = left + 'px';
-    this.#redraw();
+    this.monthlyActiveUserValue.style.left = left + 'px';
+    this._redraw();
   }
 
-  #handleStateChange() {
-    this.#loadState();
-    this.#redraw();
+  _handleStateChange() {
+    this._loadState();
+    this._redraw();
   }
 
-  #handleStepClick(event) {
+  _handleStepClick(event) {
     event.stopPropagation();
     event.preventDefault();
     if (event.currentTarget.dataset.step === 'back') {
@@ -155,98 +135,98 @@ class FusionAuthPriceCalculator {
       return;
     }
 
-    this.#step = event.currentTarget.dataset.step;
+    this.step = event.currentTarget.dataset.step;
     if (event.currentTarget.dataset.hosting) {
-      this.#hosting = event.currentTarget.dataset.hosting;
+      this.hosting = event.currentTarget.dataset.hosting;
     }
     if (event.currentTarget.dataset.plan) {
-      this.#plan = event.currentTarget.dataset.plan;
+      this.plan = event.currentTarget.dataset.plan;
     }
-    this.#changeStep();
-    window.history.pushState({}, '', this.#url);
+    this._changeStep();
+    window.history.pushState({}, '', this.url);
   }
 
-  #loadState() {
-    this.#url = new URL(window.location);
-    this.#step = this.#url.searchParams.get('step');
-    this.#hosting = this.#url.searchParams.get('hosting');
-    this.#plan = this.#url.searchParams.get('plan');
-    if (this.#step === null) {
-      this.#step = 'start';
+  _loadState() {
+    this.url = new URL(window.location);
+    this.step = this.url.searchParams.get('step');
+    this.hosting = this.url.searchParams.get('hosting');
+    this.plan = this.url.searchParams.get('plan');
+    if (this.step === null) {
+      this.step = 'start';
     }
   }
 
-  #redraw() {
+  _redraw() {
     document.querySelectorAll(`div[data-step]`).forEach(e => e.style.display = 'none');
-    document.querySelector(`div[data-step=${this.#step}]`).style.display = 'block';
+    document.querySelector(`div[data-step=${this.step}]`).style.display = 'block';
 
-    this.#hostingSelectionDiv.innerText = this.#hosting ? FusionAuthPriceCalculator.names[this.#hosting] : '-';
-    this.#planSelectionDiv.innerText = this.#plan ? this.#plan : '-';
+    this.hostingSelectionDiv.innerText = this.hosting ? FusionAuthPriceCalculator.names[this.hosting] : '-';
+    this.planSelectionDiv.innerText = this.plan ? this.plan : '-';
 
-    if (this.#hosting) {
-      this.#hostingPrice = this.#calculateHostingPrice(this.#hosting);
-      this.#hostingPriceDiv.innerText = '$' + new Intl.NumberFormat('en').format(Math.floor(this.#hostingPrice));
+    if (this.hosting) {
+      this.hostingPrice = this._calculateHostingPrice(this.hosting);
+      this.hostingPriceDiv.innerText = '$' + new Intl.NumberFormat('en').format(Math.floor(this.hostingPrice));
     } else {
-      this.#hostingPrice = 0;
-      this.#hostingPriceDiv.innerText = '-';
+      this.hostingPrice = 0;
+      this.hostingPriceDiv.innerText = '-';
     }
 
-    if (this.#hosting === 'ha-cloud') {
-      this.#communityButton.href = '#';
-      this.#communityButton.innerText = 'Not available';
-      this.#communityButton.classList.add('grayed-out');
-      this.#communityButton.classList.remove('sales');
-      this.#communityButton.setAttribute('disabled', 'disabled');
-      this.#starterButton.innerText = 'Not available';
-      this.#starterButton.setAttribute('disabled', 'disabled');
-      this.#starterButton.classList.add('grayed-out');
-      this.#starterButton.classList.remove('sales');
-    } else if (this.#hosting !== 'self-hosting') {
-      this.#communityButton.href = '#';
-      this.#communityButton.innerText = 'Select';
-      this.#communityButton.classList.remove('grayed-out');
-      this.#communityButton.classList.add('sales');
-      this.#communityButton.removeAttribute('disabled');
-      this.#starterButton.innerText = 'Select';
-      this.#starterButton.removeAttribute('disabled');
-      this.#starterButton.classList.remove('grayed-out');
-      this.#starterButton.classList.add('sales');
+    if (this.hosting === 'ha-cloud') {
+      this.communityButton.href = '#';
+      this.communityButton.innerText = 'Not available';
+      this.communityButton.classList.add('grayed-out');
+      this.communityButton.classList.remove('sales');
+      this.communityButton.setAttribute('disabled', 'disabled');
+      this.starterButton.innerText = 'Not available';
+      this.starterButton.setAttribute('disabled', 'disabled');
+      this.starterButton.classList.add('grayed-out');
+      this.starterButton.classList.remove('sales');
+    } else if (this.hosting !== 'self-hosting') {
+      this.communityButton.href = '#';
+      this.communityButton.innerText = 'Select';
+      this.communityButton.classList.remove('grayed-out');
+      this.communityButton.classList.add('sales');
+      this.communityButton.removeAttribute('disabled');
+      this.starterButton.innerText = 'Select';
+      this.starterButton.removeAttribute('disabled');
+      this.starterButton.classList.remove('grayed-out');
+      this.starterButton.classList.add('sales');
     } else {
-      this.#communityButton.href = '/download';
-      this.#communityButton.innerText = 'Download';
+      this.communityButton.href = '/download';
+      this.communityButton.innerText = 'Download';
     }
 
     document.querySelectorAll('div[data-plan]').forEach(e => e.style.border = '2px solid transparent');
-    if (this.#plan) {
-      this.#planPrice = this.#calculatePlanPrice(this.#plan);
-      this.#planPriceDiv.innerText = '$' + new Intl.NumberFormat('en').format(Math.floor(this.#planPrice));
-      document.querySelector(`div[data-plan=${this.#plan}]`).style.border = '2px solid #f58320';
+    if (this.plan) {
+      this.planPrice = this._calculatePlanPrice(this.plan);
+      this.planPriceDiv.innerText = '$' + new Intl.NumberFormat('en').format(Math.floor(this.planPrice));
+      document.querySelector(`div[data-plan=${this.plan}]`).style.border = '2px solid #f58320';
     } else {
-      this.#planPrice = 0;
-      this.#planPriceDiv.innerText = '-';
+      this.planPrice = 0;
+      this.planPriceDiv.innerText = '-';
     }
 
-    if (this.#hosting && this.#plan) {
-      this.#purchaseButton.removeAttribute('disabled');
-      this.#purchaseButton.classList.remove('grayed-out');
-      this.#purchaseButton.setAttribute('href', `https://account.fusionauth.io/account/purchase/start?hosting=${this.#hosting}&plan=${this.#plan}`)
+    if (this.hosting && this.plan) {
+      this.purchaseButton.removeAttribute('disabled');
+      this.purchaseButton.classList.remove('grayed-out');
+      this.purchaseButton.setAttribute('href', `https://account.fusionauth.io/account/purchase/start?hosting=${this.hosting}&plan=${this.plan}`)
     } else {
-      this.#purchaseButton.setAttribute('disabled', 'disabled');
-      this.#purchaseButton.classList.add('grayed-out');
+      this.purchaseButton.setAttribute('disabled', 'disabled');
+      this.purchaseButton.classList.add('grayed-out');
     }
 
-    if ((!this.#hosting || this.#hosting === 'self-hosting' || this.#hosting === 'basic-cloud') && (!this.#plan || this.#plan === 'Community' || this.#plan === 'Starter')) {
-      this.#sumDiv.innerText = 'Free trial';
-      this.#sumDiv.nextSibling.style.display = 'none';
-      this.#purchaseButton.innerHTML = 'Start trial';
-    } else if (this.#hostingPrice !== 0 || this.#planPrice !== 0) {
-      this.#sumDiv.innerText = '$' + new Intl.NumberFormat('en').format(Math.floor(this.#hostingPrice + this.#planPrice));
-      this.#sumDiv.nextSibling.style.display = 'inline';
-      this.#purchaseButton.innerHTML = 'Buy online';
+    if ((!this.hosting || this.hosting === 'self-hosting' || this.hosting === 'basic-cloud') && (!this.plan || this.plan === 'Community' || this.plan === 'Starter')) {
+      this.sumDiv.innerText = 'Free trial';
+      this.sumDiv.nextElementSibling.style.display = 'none';
+      this.purchaseButton.innerHTML = 'Start trial';
+    } else if (this.hostingPrice !== 0 || this.planPrice !== 0) {
+      this.sumDiv.innerText = '$' + new Intl.NumberFormat('en').format(Math.floor(this.hostingPrice + this.planPrice));
+      this.sumDiv.nextElementSibling.style.display = 'inline';
+      this.purchaseButton.innerHTML = 'Buy online';
     } else {
-      this.#sumDiv.innerText = '-';
-      this.#sumDiv.nextSibling.style.display = 'inline';
-      this.#purchaseButton.innerHTML = 'Buy online';
+      this.sumDiv.innerText = '-';
+      this.sumDiv.nextElementSibling.style.display = 'inline';
+      this.purchaseButton.innerHTML = 'Buy online';
     }
   }
 }
