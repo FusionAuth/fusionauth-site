501113a0cc8a70cc3767f4d42a8b982f1a99e082
Brian Pontarelli <brian@fusionauth.io>
2023-04-29 14:22:25 -0600

    Working

--- astro/src/diagrams/articles/authentication/spa/_includes.js
+++ astro/src/diagrams/articles/authentication/spa/_includes.js
@@ -54,26 +54,26 @@ export const forumsJWTLocalStorageLoad = `
 
 export const forumsJWTCookieLoad = `
   Note over Browser,Hacker: Forums load
-  Browser->>Forums: AJAX GET /api/load-load-posts<br/>[JWT HttpOnly w/ domain: Forums.Example.com]
+  Browser->>Forums: AJAX GET /api/load-load-posts<br/>[JWT HttpOnly w/ domain: forums.example.com]
   Forums->>Browser: (Forum posts)
 `;
 
 export const forumsRefreshJWTLoad = `
   Note over Browser,Hacker: Forum load
-  Browser->>Forums: AJAX GET /api/load-posts<br/>[Refresh token and JWT HttpOnly w/ domain: Forums.Example.com]
+  Browser->>Forums: AJAX GET /api/load-posts<br/>[Refresh token and JWT HttpOnly w/ domain: forums.example.com]
   Forums->>Browser: (Forum posts)
 `;
 
 export const forumsSessionLoad = `
   Note over Browser,Hacker: Forum load
-  Browser->>Forums: AJAX GET /api/load-posts<br/>[SessionId HttpOnly w/ domain: Forums.Example.com]
+  Browser->>Forums: AJAX GET /api/load-posts<br/>[SessionId HttpOnly w/ domain: forums.example.com]
   Forums->>Forums: Session extended
   Forums->>Browser: (Forum posts)
 `;
 
 export const forumsSessionRefreshLoad = `
   Note over Browser,Hacker: Forum load
-  Browser->>Forums: AJAX GET /api/load-posts<br/>[SessionId and Refresh token HttpOnly w/ domain: Forums.Example.com]
+  Browser->>Forums: AJAX GET /api/load-posts<br/>[SessionId and Refresh token HttpOnly w/ domain: forums.example.com]
   Forums->>Forums: Session extended
   Forums->>Browser: (Forum posts)
 `;
@@ -102,13 +102,13 @@ export const shoppingCartJWTLocalStorageLoad = `
 
 export const shoppingCartJWTCookieLoad = `
   Note over Browser,Hacker: Shopping cart load
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[JWT HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[JWT HttpOnly w/ domain: store.example.com]
   Store->>Browser: (Shopping cart contents)
 `;
 
 export const shoppingCartRefreshJWTLoad = `
   Note over Browser,Hacker: Shopping cart load
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[Refresh token and JWT HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[Refresh token and JWT HttpOnly w/ domain: store.example.com]
   Store->>Browser: (Shopping cart contents)
 `;
 
@@ -116,17 +116,17 @@ export const shoppingCartRefreshJWTRefresh = `
   ${jwtExpires}
   
   Note over Browser,Hacker: Shopping cart load
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[Refresh token and JWT HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[Refresh token and JWT HttpOnly w/ domain: store.example.com]
   Store->>FusionAuth: POST /oauth2/token or POST /api/jwt/refresh<br/>(grant_type=refresh and refresh token)
   FusionAuth->>Store: (JWT)
-  Store->>Browser: (Shopping cart contents)<br/>[New JWT HttpOnly w/ domain: Store.Example.com]
+  Store->>Browser: (Shopping cart contents)<br/>[New JWT HttpOnly w/ domain: store.example.com]
 `;
 
 export const shoppingCartRefreshJWTRelogin = `
   ${refreshTokenExpires}
   
   Note over Browser,Hacker: Re-login
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[Refresh token and JWT HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[Refresh token and JWT HttpOnly w/ domain: store.example.com]
   Store->>FusionAuth: POST /oauth2/token or POST /api/jwt/refresh<br/>(grant_type=refresh and refresh token)
   FusionAuth->>Store: 404 Missing
   Store->>Browser: 401 Not Authorized
@@ -135,14 +135,14 @@ export const shoppingCartRefreshJWTRelogin = `
 
 export const shoppingCartSessionLoad = `
   Note over Browser,Hacker: Shopping cart load
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId HttpOnly w/ domain: store.example.com]
   Store->>Store: Session extended
   Store->>Browser: (Shopping cart contents)
 `;
 
 export const shoppingCartSessionRefreshLoad = `
   Note over Browser,Hacker: Shopping cart load
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId and Refresh token HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId and Refresh token HttpOnly w/ domain: store.example.com]
   Store->>Store: Session extended
   Store->>Browser: (Shopping cart contents)
 `;
@@ -151,18 +151,18 @@ export const shoppingCartSessionRefreshRefresh = `
   Note over Browser: Session expires<br/>Refresh token still valid
   
   Note over Browser,Hacker: Shopping cart load
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId and Refresh token HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId and Refresh token HttpOnly w/ domain: store.example.com]
   Store->>FusionAuth: POST /oauth2/token or POST /api/jwt/refresh<br/>(grant_type=refresh and refresh token)
   FusionAuth->>Store: (JWT)
   Store->>Store: Create new session
-  Store->>Browser: (Shopping cart contents)<br/>[New SessionId HttpOnly w/ domain: Store.Example.com]
+  Store->>Browser: (Shopping cart contents)<br/>[New SessionId HttpOnly w/ domain: store.example.com]
 `;
 
 export const shoppingCartSessionRefreshRelogin = `
   Note over Browser: Session expires<br/>Refresh token expires
   
   Note over Browser,Hacker: Re-login
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId and Refresh token HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId and Refresh token HttpOnly w/ domain: store.example.com]
   Store->>FusionAuth: POST /oauth2/token or POST /api/jwt/refresh<br/>(grant_type=refresh and refresh token)
   FusionAuth->>Store: 404 Missing
   Store->>Browser: 401 Not Authorized
@@ -173,37 +173,37 @@ export const shoppingCartSessionRelogin = `
   ${sessionExpires}
   
   Note over Browser,Hacker: Re-login
-  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId HttpOnly w/ domain: Store.Example.com]
+  Browser->>Store: AJAX GET /api/load-shopping-cart<br/>[SessionId HttpOnly w/ domain: store.example.com]
   Store->>Browser: 401 Not Authorized
   Browser->>Browser: Login same as above
 `;
 
 export const stolenJWTRefreshJWT = `
   Note over Browser,Hacker: Stolen JWT
-  Hacker->>Store: GET /api/load-shopping-cart<br/>[JWT HttpOnly w/ domain: Store.Example.com]
+  Hacker->>Store: GET /api/load-shopping-cart<br/>[JWT HttpOnly w/ domain: store.example.com]
   Store->>Hacker: (Shopping cart contents)
 `;
 
 export const stolenRefreshTokenRefreshJWT = `
   Note over Browser,Hacker: Stolen refresh token
-  Hacker->>Store: GET /api/load-shopping-cart<br/>[Refresh token and bad JWT HttpOnly w/ domain: Store.Example.com]
+  Hacker->>Store: GET /api/load-shopping-cart<br/>[Refresh token and bad JWT HttpOnly w/ domain: store.example.com]
   Store->>FusionAuth: POST /oauth2/token or POST /api/jwt/refresh<br/>(grant_type=refresh and refresh token)
   FusionAuth->>Store: (JWT)
-  Store->>Hacker: (Shopping cart contents)<br/>[New JWT HttpOnly w/ domain: Store.Example.com]
+  Store->>Hacker: (Shopping cart contents)<br/>[New JWT HttpOnly w/ domain: store.example.com]
 `;
 
 export const stolenSessionId = `
   Note over Browser,Hacker: Stolen session id
-  Hacker->>Store: GET /api/load-shopping-cart<br/>[SessionId HttpOnly w/ domain: Store.Example.com]
+  Hacker->>Store: GET /api/load-shopping-cart<br/>[SessionId HttpOnly w/ domain: store.example.com]
   Store->>Store: Session extended
   Store->>Hacker: (Shopping cart contents)
 `;
 
 export const stolenSessionRefreshToken = `
   Note over Browser,Hacker: Stolen refresh token
-  Hacker->>Store: GET /shopping-cart<br/>[Refresh token and bad session id HttpOnly w/ domain: Store.Example.com]
+  Hacker->>Store: GET /shopping-cart<br/>[Refresh token and bad session id HttpOnly w/ domain: store.example.com]
   Store->>FusionAuth: POST /oauth2/token or POST /api/jwt/refresh<br/>(grant_type=refresh and refresh token)
   FusionAuth->>Store: (JWT)
   Store->>Store: Create session and store User in
-  Store->>Hacker: (Shopping cart contents)<br/>[New session id HttpOnly w/ domain: Store.Example.com]
+  Store->>Hacker: (Shopping cart contents)<br/>[New session id HttpOnly w/ domain: store.example.com]
 `;
