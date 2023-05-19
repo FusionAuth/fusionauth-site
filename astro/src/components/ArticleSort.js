dacf46c46288af4a0e459c7e423d6377ed0c49ce
Dan Moore <dan@fusionauth.io>
2023-05-19 10:30:11 -0600

    updating comment to reflect reality

--- astro/src/components/ArticleSort.js
+++ astro/src/components/ArticleSort.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// sort by sortOrder descending unless equal, then by title descending
+// sort by featured descending unless equal, then by title descending, unless title overridden by sortTitle, which allows for finer grained sorting, in which case use sortTitle
 export function sortArticle(a,b) {
   const titleToSortByForA = typeof(a.data.sortTitle) !== 'undefined' ? a.data.sortTitle : a.data.title
   const titleToSortByForB = typeof(b.data.sortTitle) !== 'undefined' ? b.data.sortTitle : b.data.title
