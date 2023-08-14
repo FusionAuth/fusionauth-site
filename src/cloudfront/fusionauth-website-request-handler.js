cd702fe33364beb66082743c3f5c377aa637dde9
Lyle Schemmerling <lyle.schemmerling@fusionauth.io>
2023-08-14 12:13:22 -0600

    no debug

--- src/cloudfront/fusionauth-website-request-handler.js
+++ src/cloudfront/fusionauth-website-request-handler.js
@@ -246,16 +246,3 @@ function calculateURI(uri) {
 function redir(loc) {
   return {statusCode:301,statusDescription:'Moved',headers:{'location':{value: loc}}}
 }
-
-console.log(handler({
-  request: {
-    uri: '/blog/author/dan-moore',
-    headers: {}
-  }
-}))
-console.log(handler({
-  request: {
-    uri: '/articles/ciam',
-    headers: {}
-  }
-}))
