216f056785147882d1f0b3a9b818cfb54466b051
Lyle Schemmerling <lyle.schemmerling@fusionauth.io>
2023-08-16 17:27:44 -0600

    oops

--- src/cloudfront/fusionauth-website-request-handler.js
+++ src/cloudfront/fusionauth-website-request-handler.js
@@ -189,7 +189,7 @@ function handler(event) {
 }
 
 function removeSlash(uri) {
-  return ip[uri] !== true && uri.match('^/blog/.*/$') &&
+  return ip[uri] !== true && uri.match('^/blog/[\\w\\d-]*/$') &&
     redirectsByPrefix.find(e => uri.startsWith(e[0])) === undefined;
 }
 
