fa354611079c11acc03e83464ff81b4fc432eacd
RichardJECooke <RichardJECooke@pm.me>
2023-09-11 17:14:50 +0200

    Temporary files

--- astro/.astro/types.d.ts
+++ astro/.astro/types.d.ts
@@ -1541,6 +1541,13 @@ declare module 'astro:content' {
   collection: "blog",
   data: any
 } & { render(): Render[".mdx"] },
+"get-more-value-out-of-fusionauth.mdx": {
+  id: "get-more-value-out-of-fusionauth.mdx",
+  slug: "get-more-value-out-of-fusionauth",
+  body: string,
+  collection: "blog",
+  data: any
+} & { render(): Render[".mdx"] },
 "getting-started-with-email-templates.mdx": {
   id: "getting-started-with-email-templates.mdx",
   slug: "getting-started-with-email-templates",
@@ -2276,6 +2283,13 @@ declare module 'astro:content' {
   collection: "blog",
   data: any
 } & { render(): Render[".mdx"] },
+"treefort-uses-fusionauth-for-all-auth.mdx": {
+  id: "treefort-uses-fusionauth-for-all-auth.mdx",
+  slug: "treefort-uses-fusionauth-for-all-auth",
+  body: string,
+  collection: "blog",
+  data: any
+} & { render(): Render[".mdx"] },
 "unio-saves-100k-with-fusionauth.mdx": {
   id: "unio-saves-100k-with-fusionauth.mdx",
   slug: "unio-saves-100k-with-fusionauth",
@@ -2542,6 +2556,20 @@ declare module 'astro:content' {
 "quickstart-golang-web.mdx": {
   id: "quickstart-golang-web.mdx",
   slug: "quickstart-golang-web",
+  body: string,
+  collection: "quickstarts",
+  data: InferEntrySchema<"quickstarts">
+} & { render(): Render[".mdx"] },
+"quickstart-javascript-express-api.mdx": {
+  id: "quickstart-javascript-express-api.mdx",
+  slug: "quickstart-javascript-express-api",
+  body: string,
+  collection: "quickstarts",
+  data: InferEntrySchema<"quickstarts">
+} & { render(): Render[".mdx"] },
+"quickstart-javascript-express-web-DELETE.mdx": {
+  id: "quickstart-javascript-express-web-DELETE.mdx",
+  slug: "quickstart-javascript-express-web-delete",
   body: string,
   collection: "quickstarts",
   data: InferEntrySchema<"quickstarts">
