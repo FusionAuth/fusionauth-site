a3e53bbeeef7b5149e2b145f373f2cb486f7babe
Dan Moore <dan@fusionauth.io>
2023-09-11 20:42:55 -0600

    Add support for different css display tags
    we need flex on the admin guide and block on the connectors page

--- site/_plugins/jekyll-mermaid-block.rb
+++ site/_plugins/jekyll-mermaid-block.rb
@@ -3,11 +3,25 @@ module Jekyll
 
     def initialize(tag_name, markup, tokens)
       super
+      @attributes = {}
+      markup.scan(::Liquid::TagAttributes) do |key, value|
+        @attributes[key] = value
+      end
+
     end
 
     def render(context)
+      display = @attributes['display']
+      puts "abbbbbbbbbbbB"
+      puts display.nil?
+      puts display
+      puts "abbbbbbbbbbbB"
+      unless display
+        display = 'flex'
+      end
+
       @config = context.registers[:site].config['mermaid']
-      "<div style=\"width: 100%;display: flex;justify-content: center;\"><div class=\"mermaid\">#{super}</div></div>"
+      "<div style=\"width: 100%;display: " + display + "; justify-content: center;\"><div class=\"mermaid\">#{super}</div></div>"
     end
   end
 end
