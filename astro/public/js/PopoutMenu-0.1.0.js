6e5e705f8accd01ff698659e8e1b6aba453d783d
Lyle Schemmerling <lyle.schemmerling@fusionauth.io>
2023-09-13 13:11:25 -0600

    Updating nav and resizing content

--- astro/public/js/PopoutMenu-0.1.0.js
+++ astro/public/js/PopoutMenu-0.1.0.js
@@ -1,74 +1,40 @@
 'use strict';
 
 class PopoutMenu {
-  #outState = ['ease-out', 'duration-100', 'opacity-0', 'scale-95'];
-  #inState = ['ease-in', 'duration-75', 'opacity-100', 'scale-100'];
+  #toggleMap = {};
 
-  #mouseOverMedButton = false;
-  #mouseOverMedMenu = false;
-  #medMenuVisible = false;
-  #medMenuClicked = false;
+  #outState = ['lg:ease-out', 'lg:duration-100', 'lg:opacity-0', 'lg:scale-95'];
+  #inState = ['lg:ease-in', 'lg:duration-75', 'lg:opacity-100', 'lg:scale-100'];
 
-  #menuElem
   constructor() {
-    this.#menuElem = document.getElementById('more-menu');
-
-    document.getElementById('menu-button').addEventListener('mouseout', event => {
-        this.#mouseOverMedButton = false;
-        if (!this.#medMenuClicked) {
-          this.#toggleMedMenu();
-        }
-      });
-
-      document.getElementById('more-menu').addEventListener('mouseout', event => {
-        this.#mouseOverMedMenu = false;
-        if (!this.#medMenuClicked) {
-          this.#toggleMedMenu();
-        }
-      });
-
-      document.getElementById('menu-button').addEventListener('mouseover', event => {
-        this.#mouseOverMedButton = true;
-        if (!this.#medMenuClicked) {
-          this.#toggleMedMenu();
-        }
-      });
-
-      document.getElementById('more-menu').addEventListener('mouseover', event => {
-        this.#mouseOverMedMenu = true;
-        if (!this.#medMenuClicked) {
-          this.#toggleMedMenu();
-        }
-      });
-
-      document.getElementById('menu-button').addEventListener('click', event => {
-        // handle mobile clicked
-        this.#medMenuClicked = !this.#medMenuClicked;
-        this.#toggleMedMenu();
-      });
+    document.addEventListener('click', () => this.#handleClick(event));
   }
 
-  #toggleMedMenu() {
-    if (this.#medMenuClicked) {
-      if (!this.#medMenuVisible) {
-        this.#medMenuVisible = true;
-        this.#menuElem.classList.remove(...this.#outState);
-        this.#menuElem.classList.add(...this.#inState);
-      }
+  #handleClick(event) {
+    const button = event.target.closest('[data-widget="popout-button"]');
+    if (!button || !button.dataset.element) {
+      return;
     }
-    if ((this.#mouseOverMedButton || this.#mouseOverMedMenu) && !this.#medMenuVisible) {
-      this.#medMenuVisible = true;
-      this.#menuElem.classList.remove(...this.#outState);
-      this.#menuElem.classList.add(...this.#inState);
-    } else if (!this.#medMenuClicked) {
-      setTimeout(() => {
-        if (!this.#mouseOverMedMenu && !this.#mouseOverMedMenu) {
-          this.#medMenuVisible = false;
-          this.#menuElem.classList.remove(...this.#inState);
-          this.#menuElem.classList.add(...this.#outState);
-        }
-      }, 500);
+
+    const id = button.dataset.element;
+    const element = document.getElementById(id);
+    this.#toggleMenuClicked(id, element);
+  }
+
+  #toggleMenuClicked(id, elem) {
+    if (!Object.keys(this.#toggleMap).includes(id)) {
+      this.#toggleMap[id] = false;
     }
+
+    if (!this.#toggleMap[id]) {
+      elem.classList.remove(...this.#outState);
+      elem.classList.add(...this.#inState);
+    } else {
+      elem.classList.remove(...this.#inState);
+      elem.classList.add(...this.#outState);
+    }
+
+    this.#toggleMap[id] = !this.#toggleMap[id];
   }
 }
 
