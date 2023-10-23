8c444862e530d36f31ce0acc0e5d7fdb7238b92a
Lyle Schemmerling <lyle.schemmerling@fusionauth.io>
2023-10-23 16:09:21 -0600

    code that is all sean's fault

--- astro/public/js/Visibility-0.1.0.js
+++ astro/public/js/Visibility-0.1.0.js
@@ -6,12 +6,23 @@ class Visibility {
   }
 
   #handleClick(event) {
+
     const button = event.target.closest('[data-widget="visibility-button"]');
     if (!button || !button.dataset.element) {
+      const navbtns = document.querySelectorAll('[data-widget="visibility-button"]');
+      navbtns.forEach(btn => {
+        const id = btn.dataset.element;
+        const nav = document.getElementById(id);
+        if (!nav.classList.contains('hidden') && !nav.contains(event.target)) {
+          nav.classList.add('hidden');
+        }
+      });
       return;
     }
 
     const element = document.getElementById(button.dataset.element);
+    event.preventDefault();
+    event.stopPropagation();
     element.classList.toggle('hidden')
   }
 }
