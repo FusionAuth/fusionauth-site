31d354eb6ebebc1c579eaf66540af3bd643370a2
John Jeffers <john.jeffers@fusionauth.io>
2023-10-26 13:12:20 -0600

    jj/redirect rules from s3 (#2611)
    * safety checkin
    
    * safety checkin
    
    * include package lock file, misc updates
    
    * workflow for s3 upload
    
    * only run workflow on push
    
    * add readme
    
    * fix cloudfront function check workflow
    
    * PR review suggestions
    
    * rename workflow

--- src/lambdas/fusionauth-site-origin-handler/test/index.test.js
+++ src/lambdas/fusionauth-site-origin-handler/test/index.test.js
@@ -1,59 +1,55 @@
-//import {test} from "node:test";
-//import assert from "node:assert/strict";
-//import handler from '../src/fusionauth-website-lambda-request-handler.js';
-const { test} = require("node:test");
-const assert = require("node:assert/strict");
-const { handler } = require('../src/fusionauth-website-lambda-request-handler.js');
+import assert from 'node:assert/strict';
+import { handler } from '../src/index.js';
 
-// fix this hack
-let callbackResult;
+let event, result;
 
-const mockCallback = (unknown, result) => { callbackResult = result; }
-
-const runTest = (pathToTest, expectedResult) => {
-  handler(makeRequest(pathToTest), null, mockCallback);
-
-  assert.deepStrictEqual(callbackResult, expectedResult);
+// Mock a lambda event that we can pass to the handler.
+const mockEvent = (path) => {
+  return { 'Records': [{ 'cf': { 'request': { 'headers': {}, 'uri': path }}}]}
 }
 
-const testPath = (path) => {
-  return handler(makeRequest(path));
-}
-
-const makeRequest = (path) => {
-  return { Records: [ { cf: { request: {headers: [], uri: path}}}]};
-}
-
+// Build the expected response for a 301 redirect.
 const makeRedirect = (path) => {
-  return {status: 301, statusDescription: 'Moved', headers: {'location': [{key: "Location", value: path}]}};
-};
+  return { 'status': 301, 'statusDescription': 'Moved', 'headers': {'location': [{ 'key': "Location", 'value': path }]}};
+}
 
+// Build the expected response for a passthrough.
 const makePassThroughRequest = (path) => {
-  return {headers: [], uri: path};
+  return { headers: {}, uri: path };
 };
 
-test('html suffix', (t) => {
+
+// All the tests follow a similar format. We pass the mock event to the
+// handler which contains the path we're testing. The handler's real
+// response is asserted to be equal to the expected response.
+const runTest = (path, expected) => {
+  it(path, async function() {
+    result = await handler(mockEvent(path), null);
+    assert.deepEqual(result, expected);
+  });
+}
+
+describe('html extension', function() {
   runTest('/something.html', makeRedirect('/something'));
 });
 
-test('docs home', (t) => {
+describe('docs root', function() {
   runTest('/docs', makeRedirect('/docs/'));
   runTest('/docs/', makePassThroughRequest('/docs/index.html'));
 });
 
-test('docs redirects', (t) => {
+describe('docs redirects', function() {
   runTest('/cognito/', makeRedirect('/cognito'));
   runTest('/cognito', makeRedirect('/docs/lifecycle/migrate-users/bulk/cognito'));
   runTest('/docs/v1/tech/installation-guide/configuration-management', makeRedirect('/docs/operate/deploy/configuration-management'));
   runTest('/features/advanced-registration-forms', makeRedirect('/platform/registration-forms'));
-
   runTest('/docs/v1/tech/common-errors', makeRedirect('/docs/operate/troubleshooting/troubleshooting'));
   runTest('/docs/v1/tech/admin-guide/troubleshooting', makeRedirect('/docs/operate/troubleshooting/troubleshooting'));
   runTest('/docs/v1/tech/reactor/', makeRedirect('/docs/v1/tech/reactor'));
   runTest('/docs/v1/tech/identity-providers/', makeRedirect('/docs/lifecycle/authenticate-users/identity-providers/'));
 });
 
-test('blog', (t) => {
+describe('blog', function() {
   runTest('/blog', makeRedirect('/blog/'));
   runTest('/blog/category/tutorial', makeRedirect('/blog/category/tutorial/'));
   runTest('/blog/category/tutorial/', makePassThroughRequest('/blog/category/tutorial/index.html'));
@@ -66,14 +62,13 @@ test('blog', (t) => {
   runTest('/blog/digitalocean-oneclick-installation', makePassThroughRequest('/blog/digitalocean-oneclick-installation.html'));
 });
 
-test('articles', (t) => {
+describe('articles', function() {
   runTest('/learn/expert-advice/', makeRedirect('/articles/'));
   runTest('/learn/expert-advice/tokens/jwt-authentication-token-components-explained', makeRedirect('/articles/tokens/jwt-components-explained'));
   runTest('/products/identity-user-management/ciam-vs-iam', makeRedirect('/articles/ciam/ciam-vs-iam'));
 });
 
-test('dev-tools', (t) => {
-
+describe('dev-tools', function() {
   runTest('/dev-tools/jwt-debugger', makeRedirect('/dev-tools/jwt-decoder'));
   runTest('/learn/expert-advice/dev-tools/this/should-be-a-prefix-replacement', makeRedirect('/dev-tools/this/should-be-a-prefix-replacement'));
 });
