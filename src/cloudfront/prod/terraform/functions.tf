3e68cf08ab48d4b228ba1ac71d3983cd4dcc9e31
John Jeffers <john.jeffers@fusionauth.io>
2023-10-31 13:46:39 -0600

    remove unused function

--- src/cloudfront/prod/terraform/functions.tf
+++ src/cloudfront/prod/terraform/functions.tf
@@ -1,14 +1,4 @@
 #
-# fusionauth-website-request-handler function - used as the viewer-request handler for most behaviors.
-#
-resource "aws_cloudfront_function" "fusionauth_website_request_handler" {
-  name    = "fusionauth-website-request-handler"
-  comment = "Request handler for the FusionAuth website"
-  runtime = "cloudfront-js-1.0"
-  code    = file("../functions/fusionauth-website-request-handler.js")
-}
-
-#
 # fusionauth-security-headers function - used as the viewer-response handler for most behaviors.
 #
 resource "aws_cloudfront_function" "fusionauth_security_headers" {
