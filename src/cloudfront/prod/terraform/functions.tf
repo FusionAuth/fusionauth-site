fc19a58e676bd343cfd5d263518c38ec4e5c9810
John Jeffers <john.jeffers@fusionauth.io>
2023-10-31 19:08:40 -0600

    remove cloudfront deploy

--- src/cloudfront/prod/terraform/functions.tf
+++
@@ -1,9 +0,0 @@
-#
-# fusionauth-security-headers function - used as the viewer-response handler for most behaviors.
-#
-resource "aws_cloudfront_function" "fusionauth_security_headers" {
-  name    = "fusionauth-security-headers"
-  comment = "Adds the security headers for the FusionAuth website."
-  runtime = "cloudfront-js-1.0"
-  code    = file("../functions/fusionauth-security-headers.js")
-}
