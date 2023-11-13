21dea983d6f913a4c92ebd613c6ca5daa61218f3
Dan Moore <dan@fusionauth.io>
2023-11-13 10:45:40 -0700

    troubleshooting

--- src/new-images-shrink.sh
+++ src/new-images-shrink.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "here"
+
 for file in `gh pr diff --name-only`; do
+  echo "processing $file"
   if [[ $file == *.png ]]; then
     ./src/shrink-images.sh $file
   fi
+  echo "done processing $file"
 done
 
