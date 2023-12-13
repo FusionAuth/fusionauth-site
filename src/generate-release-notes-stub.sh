a6e09132c5111414ad33ea9e22219af90bfea748
Lyle Schemmerling <lyle.schemmerling@fusionauth.io>
2023-12-13 11:52:48 -0700

    post migration release notes script (#2682)

--- src/generate-release-notes-stub.sh
+++ src/generate-release-notes-stub.sh
@@ -15,23 +15,20 @@ fi
 milestone=$1
 date=$(date +"%B %-d, %Y" | sed 's/1,/1st,/;s/2,/2nd,/;s/3,/3rd,/;s/\([0-9]\),/\1th,/')
 
-echo "[role=release-note]"
+echo "<ReleaseNoteHeading version='${milestone}' releaseDate='${date}' />"
 echo ""
-echo "== Version ${milestone}"
-echo "_${date}_"
+echo "<DatabaseMigrationWarning/>"
 echo ""
-echo "include::docs/v1/tech/__database-migration-warning.adoc[]"
-echo ""
-echo "=== Known Issues"
+echo "### Known Issues"
 echo ""
 
 gh_issue_list() {
     label=$1
     header=$2
-    echo "=== $header"
+    echo "### $header"
     set -o noglob # don't be globbing files in my backticks
 
-    gh issue list --repo FusionAuth/fusionauth-issues -m $milestone -L 250 --label $label --search "sort:created-asc" --json number,body,title --jq ".[]|[.number,.body,.title] | @tsv" |
+    gh issue list --repo FusionAuth/fusionauth-issues -m $milestone --state all -L 250 --label $label --search "sort:created-asc" --json number,body,title --jq ".[]|[.number,.body,.title] | @tsv" |
     while IFS=$'\t' read -r number body title; do
         generated_copy=`echo $body | awk -F'### Release Notes' '{print $2}'`
         if [[ "${generated_copy}" == "" ]]; then
@@ -40,7 +37,7 @@ gh_issue_list() {
             generated_copy=`echo $generated_copy | sed 's#\\\\r\\\\n#\n#g'`
         fi
         echo "* ${generated_copy#[$'\r\t\n ']}"
-        echo "** Resolves https://github.com/FusionAuth/fusionauth-issues/issues/$number[GitHub Issue #$number]"
+        echo "  * Resolves [GitHub Issue #$number](https://github.com/FusionAuth/fusionauth-issues/issues/$number)"
     done
     echo ""
 }
