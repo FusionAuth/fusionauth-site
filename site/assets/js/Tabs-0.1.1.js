421a9607c1520fa6f3c64fd305dcfbabfad7b6a5
Lyle Schemmerling <lyle.schemmerling@fusionauth.io>
2023-12-13 12:03:09 -0700

    DELETE ALL THE THINGS

--- site/assets/js/Tabs-0.1.1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Prime.Document.onReady(function() {
-  Prime.Document.query('.tabs').each(e => new Prime.Widgets.Tabs(e)
-    .withLocalStorageKey(e.getDataAttribute('tabStorageKey') || null)
-    .withTabContentClass('active')
-    .initialize());
-});
