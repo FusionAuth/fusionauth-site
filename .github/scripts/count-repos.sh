37134839f0886693a96014ca98787f1968ef2a72
Alex Patterson <139158618+alex-fusionauth@users.noreply.github.com>
2024-02-27 14:24:45 -0500

    fix broken anchors (#2915)
    Co-authored-by: Alex Patterson <alex@fusionauth.io>

--- .github/scripts/count-repos.sh
+++ .github/scripts/count-repos.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-SHOW_DIFF=0
-if [ "$1x" == "-vx" ]; then
-  SHOW_DIFF=1
-fi
-
 # count the repos we have in our example apps page and the number we have in our organization and make sure they match
 
 #fusionauth-containers, fusionauth-theme-helper, etc
 EXTRA_IN_JSON_NOT_NAMED_CORRECTLY=4 
 
 # fusionauth-example-template and (temporarily) fusionauth-example-vue-sdk
-EXTRA_IN_GH_NOT_DISPLAYABLE=2
+EXTRA_IN_GH_NOT_DISPLAYABLE=3
 
 cat astro/src/content/json/exampleapps.json|jq '.[]|.url' |sed 's/"//g'|sed 's!https://github.com/!!i' > json.list
 COUNT_IN_JSON=`wc -l json.list |sed 's/^ *//' |sed 's/ .*//'`
@@ -22,10 +17,8 @@ COUNT_EXAMPLE_REPOS=`wc -l ex.list |sed 's/^ *//' |sed 's/ .*//'`
 gh repo list fusionauth --no-archived --visibility public -L 300 |grep fusionauth-quickstart | sed 's/\t.*//g'> qs.list
 COUNT_QUICKSTART_REPOS=`wc -l qs.list |sed 's/^ *//' |sed 's/ .*//'`
 
-if [ $SHOW_DIFF -eq 1 ]; then
-  sort json.list > json.sorted
-  sort ex.list qs.list > gh.sorted
-  diff json.sorted gh.sorted
-fi
+sort json.list > json.sorted
+sort ex.list qs.list > gh.sorted
+diff json.sorted gh.sorted
 
 echo "$COUNT_QUICKSTART_REPOS + $COUNT_EXAMPLE_REPOS - $EXTRA_IN_GH_NOT_DISPLAYABLE - $COUNT_IN_JSON + $EXTRA_IN_JSON_NOT_NAMED_CORRECTLY"|bc
