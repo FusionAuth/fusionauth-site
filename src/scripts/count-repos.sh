d4cb88b23f50b6c2569d2fb7cbe6cc8cf714bcb5
Dan Moore <dan@fusionauth.io>
2024-08-07 11:14:27 -0400

    adding qr code example to ignore list to stop breaking the script (#3189)

--- src/scripts/count-repos.sh
+++ src/scripts/count-repos.sh
@@ -34,6 +34,7 @@ function get_json_repos() {
   # Repos that will be returned by the `gh` commands below, but are
   # intentionally excluded from JSONFILE for... reasons?
   local EXCLUDE=(
+    "fusionauth-example-example-qr-codes"
     "fusionauth-example-node-centralized-sessions"
     "fusionauth-example-template"
     "fusionauth-example-vue-sdk"
