8bd9ecd6aadbbef0809135b2a2f9aa9f1159a433
Lyle Schemmerling <lshem16@gmail.com>
2024-11-22 16:14:20 -0700

    better html checker

--- astro/.remarkrc.js
+++ astro/.remarkrc.js
@@ -3,26 +3,39 @@ import remarkPresetLintRecommended from 'remark-preset-lint-recommended';
 import { lintRule } from 'unified-lint-rule';
 import { visitParents } from 'unist-util-visit-parents';
 
+const checkIfHtml = (node, parents, file) => {
+  if (!node.position) return
+
+  if (/^[\t ]*<!--/.test(node.value)) return
+
+  const name = node.name;
+  // check if name starts with a capital letter
+  const isComponent = /^[A-Z]/.test(name);
+  if (isComponent) return;
+
+  file.message(`Unexpected HTML tag [${name}], use markdown instead`, {
+    ancestors: [...parents, node],
+    place: node.position
+  })
+}
+
 const noHtml = lintRule(
     {
       origin: 'remark-lint:no-html',
     },
 
     function (tree, file) {
+      //console.log(tree);
       visitParents(tree, 'mdxJsxFlowElement', function (node, parents) {
-        if (!node.position) return
+        checkIfHtml(node, parents, file);
+      })
 
-        if (/^[\t ]*<!--/.test(node.value)) return
-
-        const name = node.name;
-        // check if name starts with a capital letter
-        const isComponent = /^[A-Z]/.test(name);
-        if (isComponent) return;
-
-        file.message(`Unexpected HTML tag [${name}], use markdown instead`, {
-          ancestors: [...parents, node],
-          place: node.position
-        })
+      visitParents(tree, 'paragraph', function (node, parents) {
+        [...node.children].forEach((child) => {
+          if (child.type === 'mdxJsxTextElement') {
+            checkIfHtml(child, parents, file);
+          }
+        });
       })
     }
 )
