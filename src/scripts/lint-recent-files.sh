066c0f807615ec1301ca8f5b57d365878dc0980d
Dan Moore <dan@fusionauth.io>
2024-12-09 09:43:22 -0700

    don't hardcode PR number

--- src/scripts/lint-recent-files.sh
+++ src/scripts/lint-recent-files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Get the list of changed files in the pull request
-changed_files=$(gh api repos/FusionAuth/fusionauth-site/pulls/3415/files -q '.[] | [.status,.filename] | @csv' |grep -v '"removed"'|awk -F, '{print $2}'|sed 's/"//g')
+changed_files=$(gh api repos/FusionAuth/fusionauth-site/pulls/$PR_NUMBER/files -q '.[] | [.status,.filename] | @csv' |grep -v '"removed"'|awk -F, '{print $2}'|sed 's/"//g')
 
 # Filter the files to match the specified patterns and directory
 files_to_check=$(echo "$changed_files" | grep -E '^astro/src/.*\.(js|mjs|cjs|ts|md|mdx)$' | sed 's|^astro/||')
