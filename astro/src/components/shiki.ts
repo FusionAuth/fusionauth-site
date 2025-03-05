ebbe4f49cbf37d6f403ea10bac45748f1c145cf5
Colin Frick <colin+github@sonderformat.llc>
2025-03-05 09:54:32 +0100

    refactor: optimize highlighter handling, add debug / log information when language is not bundled

--- astro/src/components/shiki.ts
+++ astro/src/components/shiki.ts
@@ -1,4 +1,4 @@
-import {bundledLanguages, createHighlighter, Highlighter} from "shiki/bundle/full";
+import {bundledLanguages, Highlighter, getSingletonHighlighter} from "shiki/bundle/full";
 
 let cachedHighlighter: Promise<Highlighter> | undefined = undefined;
 const cachedLoadedLanguages = new Map<string, Promise<void>>();
@@ -17,11 +17,11 @@ const HIGHLIGHTER_CONFIG = {
  *
  * @return {Promise<Highlighter>} A promise that resolves to the cached Highlighter instance.
  */
-export function getCachedHighlighter(): Promise<Highlighter> {
-  if (!cachedHighlighter) {
-    cachedHighlighter = createHighlighter(HIGHLIGHTER_CONFIG);
-  }
-  return cachedHighlighter;
+export async function getCachedHighlighter(): Promise<Highlighter> {
+  if (cachedHighlighter) await cachedHighlighter;
+
+  cachedHighlighter = getSingletonHighlighter(HIGHLIGHTER_CONFIG);
+  return await cachedHighlighter;
 }
 
 /**
@@ -31,9 +31,10 @@ export function getCachedHighlighter(): Promise<Highlighter> {
  *
  * @param {Highlighter} highlighter - The highlighter instance responsible for managing and applying syntax highlighting.
  * @param {string} language - The identifier of the language to ensure is loaded.
+ * @param {unknown} data - Any data relevant for logging or debugging purposes.
  * @return {Promise<void>} - A promise that resolves once the specified language is confirmed to be loaded.
  */
-export function ensureLanguageIsLoaded(highlighter: Highlighter, language: string): Promise<void> {
+export function ensureLanguageIsLoaded(highlighter: Highlighter, language: string, data: unknown): Promise<void> {
   const cachedPromise = cachedLoadedLanguages.get(language);
   if (cachedPromise) {
     return cachedPromise;
@@ -41,6 +42,10 @@ export function ensureLanguageIsLoaded(highlighter: Highlighter, language: strin
 
   const isLanguageAlreadyLoaded = highlighter.getLoadedLanguages().includes(language);
   const isBundledLanguage = Object.keys(bundledLanguages).includes(language);
+
+  if (!isBundledLanguage) {
+    console.warn(`[Shiki] Language "${language}" is not a bundled language. ${JSON.stringify(data)}`);
+  }
 
   const loadPromise = isLanguageAlreadyLoaded || !isBundledLanguage
     ? Promise.resolve() : highlighter.loadLanguage(bundledLanguages[language]);
