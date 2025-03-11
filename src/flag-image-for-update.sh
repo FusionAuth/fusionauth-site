35bb7472b6d44abbad4d3bd08e2b799627d73c93
Brent Halsey <brent.halsey@fusionauth.io>
2025-03-11 10:02:33 -0600

    Update src/flag-image-for-update.sh
    Co-authored-by: Spencer Witt <spencer@fusionauth.io>

--- src/flag-image-for-update.sh
+++ src/flag-image-for-update.sh
@@ -3,7 +3,7 @@
 # Script to mark screenshots that need to be udpated for a feature branch 
 # (but we want to wait until all UI changes are done)
 #
-# Overlays transparent red text "update for issue1" to make it visible
+# Overlays transparent red text "Needs Update" to make it visible
 # (the image size becomes larger, but then a GHA will shrink it)
 #
 # Usage:
