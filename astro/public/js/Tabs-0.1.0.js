bee23e7144f68567799639a768c25202816679c0
Brian Pontarelli <brian@fusionauth.io>
2025-03-20 13:30:07 -0600

    Breaking out some new components to make life simpler. Also, reworking the Docker so it works on Windows (why not right?)

--- astro/public/js/Tabs-0.1.0.js
+++ astro/public/js/Tabs-0.1.0.js
@@ -18,6 +18,18 @@ class Tabs {
       const buttons = anchor.closest('ul').querySelectorAll('a[data-tab-button]');
       buttons.forEach(button => {
         button.parentElement.classList.remove('active');
+
+        // Hide all the tabs
+        var href = button.getAttribute('href');
+        if (href === null) {
+          return;
+        }
+        href = href.substring(1);
+
+        const div = document.getElementById(href);
+        if (div) {
+          div.classList.add('hidden');
+        }
       });
       anchor.parentElement.classList.add('active');
 
@@ -28,15 +40,8 @@ class Tabs {
       }
 
       // Find the div
-      console.log(href);
       const div = document.getElementById(href);
-      console.log(div);
-      if (div !== null && div.matches('[data-tab-content')) {
-        const tabs = div.parentElement.querySelectorAll(':scope > [data-tab-content]')
-        tabs.forEach(tab => {
-          tab.classList.add('hidden');
-        });
-
+      if (div) {
         div.classList.remove('hidden');
       }
     }
