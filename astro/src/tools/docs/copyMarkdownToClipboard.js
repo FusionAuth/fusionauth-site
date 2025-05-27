f6f72252e061c549353c4bb8a893e6a4d2805080
Dan Moore <dan@fusionauth.io>
2025-05-27 14:46:33 -0600

    js cleanup

--- astro/src/tools/docs/copyMarkdownToClipboard.js
+++ astro/src/tools/docs/copyMarkdownToClipboard.js
@@ -1,6 +1,11 @@
 const button = document.querySelector('#copy-docs-markdown-llm-button');
 button.addEventListener('click',async event => {
   const href = button.dataset.href;
+  const resetTextMS = 2000;
+
+  // if you change this, change the initial value in the astro component as well
+  const btnText = 'Copy as Markdown for LLMs';
+
   try {
     const response = await fetch(href);
     if (!response.ok) throw new Error('Failed to fetch file');
@@ -12,13 +17,13 @@ button.addEventListener('click',async event => {
 
     // Optionally reset button text after 2 seconds
     setTimeout(() => {
-      button.textContent = 'Copy File Contents';
-    }, 2000);
+      button.textContent = btnText;
+    }, resetTextMS);
   } catch (err) {
     console.error('Error copying file contents:', err);
     button.textContent = 'Failed';
     setTimeout(() => {
-      button.textContent = 'Copy File Contents';
-    }, 2000);
+      button.textContent = btnText;
+    }, resetTextMS);
   }
 });
