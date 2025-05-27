cb8001f78a89535cae55a1b1b034fb283eeb1611
Dan Moore <dan@fusionauth.io>
2025-05-27 11:57:57 -0600

    refactoring as suggested by Lyle

--- astro/src/tools/docs/copy-md-to-clipboard.js
+++ astro/src/tools/docs/copy-md-to-clipboard.js
@@ -1,35 +1,24 @@
-'use strict';
+const button = document.querySelector('#copyBtn');
+button.addEventListener(async event => {
+  const href = button.dataset.href;
+  try {
+    const response = await fetch(href);
+    if (!response.ok) throw new Error('Failed to fetch file');
 
-class CopyMarkdownToClipboard {
-  constructor() {
-    document.addEventListener('click', event => this.#handleClick(event));
+    const text = await response.text();
+
+    await navigator.clipboard.writeText(text);
+    button.textContent = 'Copied';
+
+    // Optionally reset button text after 2 seconds
+    setTimeout(() => {
+      button.textContent = 'Copy File Contents';
+    }, 2000);
+  } catch (err) {
+    console.error('Error copying file contents:', err);
+    button.textContent = 'Failed';
+    setTimeout(() => {
+      button.textContent = 'Copy File Contents';
+    }, 2000);
   }
-
-  async #handleClick(event) {
-    const button = event.target;
-    const href = button.getAttribute('data-href');
-
-    try {
-      const response = await fetch(href);
-      if (!response.ok) throw new Error('Failed to fetch file');
-  
-      const text = await response.text();
-  
-      await navigator.clipboard.writeText(text);
-      button.textContent = 'Copied';
-  
-      // Optionally reset button text after 2 seconds
-      setTimeout(() => {
-        button.textContent = 'Copy File Contents';
-      }, 2000);
-    } catch (err) {
-      console.error('Error copying file contents:', err);
-      button.textContent = 'Failed';
-      setTimeout(() => {
-        button.textContent = 'Copy File Contents';
-      }, 2000);
-    }
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => new CopyMarkdownToClipboard());
+});
