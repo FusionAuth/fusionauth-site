d22307781dbc6b21e3582ff1521abdcc626a3acf
nathan contino <nathan.contino@fusionauth.io>
2026-04-01 15:50:48 +0000

    Normalize nav across categories (#4215)
    * Consolidate dev-tools and quickstarts into docs collection, redirect everything to new locations
    
    * Articles sidebar improvements
    
    * Fix articles collection
    
    * Add new redirects
    
    * Move quickstarts into get-started, move dev-tools to bottom, update redirects
    
    * Tweak sidebar to support multiple lines; fix redirect formatting
    
    * Add new ChildCards component to generate cards for current page across any collection
    
    * Fix broken links to quickstarts, convert md files to mdx so astro handles them nicely
    
    * Remove .html suffix from Astro.url.pathname in production build
    
    * Fix some trailing slashes
    
    * Explicit ordering for quickstart sections

--- astro/src/tools/docs/filterDocs.ts
+++
@@ -1,4 +0,0 @@
-import { filterByPath } from 'src/tools/docs/filterByPath';
-import { filterPartials } from 'src/tools/docs/filterPartials';
-
-export const filterDocs = (entry, path) => filterPartials(entry) && filterByPath(entry, path);
