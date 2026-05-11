3635637f3e04d258c1a4067a4ea8c3dcc8ad6d31
Gareth Dwyer <gareth@ritza.co>
2026-05-11 14:21:47 +0200

    support multiple algolia indices, simplify client transforms for new index

--- astro/src/config.ts
+++ astro/src/config.ts
@@ -1,5 +1,5 @@
 export const algoliaConfig = {
   appId: 'CEDVXLJPHK',
   apiKey: 'ec72d3111c6c6effa6c80b79636b9ba7',
-  indexName: 'Fusionauth Docs',
+  indexName: import.meta.env.PUBLIC_ALGOLIA_INDEX || 'Fusionauth Docs',
 };
