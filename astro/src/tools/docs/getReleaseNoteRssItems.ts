fb58398e0d9c6e190761e564f64d625538560eca
Nathan Contino <nathan.contino@fusionauth.io>
2026-05-12 10:37:55 -0400

    Migrate remaining releases from archive to improved notes format

--- astro/src/tools/docs/getReleaseNoteRssItems.ts
+++ astro/src/tools/docs/getReleaseNoteRssItems.ts
@@ -3,32 +3,8 @@ import { getCollection } from 'astro:content';
 
 
 export const getReleaseNoteRssItems = async () => {
-  const archive = await getEntry('docs', 'release-notes/archive');
-  const lines = archive.body.split("\n");
-  const items = lines.map(line => line.match(/ReleaseNoteHeading version=['"]([^('|")]*)['"] releaseDate=['"]([^('|")]*)['"]/))
-      .filter(line => !!line)
-      .map(match => ({version: match[1], date: match[2]}))
-      .map(version => ({
-        version: version.version,
-        versionRef: version.version.replaceAll('.', '-'),
-        date: new Date(version.date.replace(/(\d+)(st|nd|rd|th)/, '$1'))
-      }))
-      .map(version => {
-        const id = `https://fusionauth.io/docs/release-notes/archive#version-${version.versionRef}`;
-        const link = id;
-        const title = `Release ${version.version}`;
-        const description = `Release ${version.version}`;
-        const updated = version.date;
-        return {
-          id,
-          link,
-          description,
-          title,
-          pubDate: updated
-        };
-      });
-  const nonArchivedReleases = await getCollection('releases');
-  const nonArchivedItems = nonArchivedReleases.map(item => {
+  const releases = await getCollection('releases');
+  const items = nonArchivedReleases.map(item => {
     const id = `https://fusionauth.io/docs/release-notes#version-${item.data.version.replace(/\./g, '-')}`;
     const link = id;
     const description = item.data.description? item.data.description : `Release ${item.data.version} includes bug fixes and performance updates.`;
