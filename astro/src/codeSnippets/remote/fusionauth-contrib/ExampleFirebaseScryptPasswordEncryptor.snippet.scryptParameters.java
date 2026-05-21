2f0c289e6b514292d6f76633fd64a2ec3d699c0b
RichardJECooke <RichardJECooke@pm.me>
2026-05-21 17:20:30 +0200

    exclude remote snippets

--- astro/src/codeSnippets/remote/fusionauth-contrib/ExampleFirebaseScryptPasswordEncryptor.snippet.scryptParameters.java
+++
@@ -1,8 +0,0 @@
-/* Firebase Scrypt Parameters. You can find the correct settings for your Firebase project
-  by opening the Firebase console, navigating to the Authentication panel, and clicking
-  on the 3 dots near the the **Add User** button. Then select **Password hash parameters**.
-  This will open a modal with the parameters needed below. Copy them here */
-private static final int memcost = 14;
-private static final int rounds = 8;
-private static final String saltSep = "Bw==";
-private static final String base64_signer_key = "GhlZmw+EylyJl3fhsIOa+bmAEP1sYSHfZargl1unu/5mCp7pi818imxZOdSjM1558T+XU3cBflyr5HDghZqv7Q==";
