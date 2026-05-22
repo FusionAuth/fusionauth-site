c21fe1502d50d40e1741f420dd2b989d7002faf2
jamesdanielwhitford <james.whitford0@gmail.com>
2026-05-22 16:52:37 +0200

    proof of concept: testable code snippets via Bluehawk

---
+++ astro/src/code-snippets/webauthn/buffer-to-base64url.js
@@ -0,0 +1,12 @@
+// :snippet-start: buffer-to-base64url
+// :state-start: published
+function bufferToBase64URL(buffer) {
+  const bytes = new Uint8Array(buffer);
+  let string = '';
+  bytes.forEach(b => string += String.fromCharCode(b));
+
+  const base64 = btoa(string);
+  return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
+// :state-end:
+// :snippet-end:
