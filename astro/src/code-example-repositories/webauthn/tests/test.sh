4a64a424b699b42bef45121294539983dd16723f
RichardJECooke <RichardJECooke@pm.me>
2026-06-11 14:12:20 +0200

    everything works.  haven't tested github workflows yet

--- astro/src/code-example-repositories/webauthn/tests/test.sh
+++ astro/src/code-example-repositories/webauthn/tests/test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v "$(pwd):/app" -w /app node:24-alpine sh -c "npm install && npm test"
+docker run --rm -v "$(pwd):/app" -w /app node:26 sh -c "npm install && npm test"
