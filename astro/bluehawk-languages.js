977a6783bf6a78990ffae7d6aa9cff729f934f53
Gareth Dwyer <sixhobbits@users.noreply.github.com>
2026-07-01 00:13:10 +0200

    Update bluehawk pipeline (#4371)

---
+++ astro/bluehawk-languages.js
@@ -0,0 +1,41 @@
+export function register({ bluehawk }) {
+  // Bluehawk 1.6.0 only includes a small built-in language set; these cover
+  // common example-file extensions that otherwise warn or fail during snip/copy.
+  bluehawk.addLanguage(["yml"], {
+    languageId: "yaml",
+    lineComments: [/#/],
+  });
+  bluehawk.addLanguage("css", {
+    languageId: "css",
+    blockComments: [[/\/\*/, /\*\//]],
+  });
+  bluehawk.addLanguage(["bash", "zsh"], {
+    languageId: "shell",
+    lineComments: [/#/],
+  });
+  bluehawk.addLanguage(["ini", "cfg", "conf", "env"], {
+    languageId: "ini",
+    lineComments: [/#/],
+  });
+  bluehawk.addLanguage("toml", {
+    languageId: "toml",
+    lineComments: [/#/],
+  });
+  bluehawk.addLanguage("sql", {
+    languageId: "sql",
+    lineComments: [/--/],
+    blockComments: [[/\/\*/, /\*\//]],
+  });
+  bluehawk.addLanguage(["graphql", "gql"], {
+    languageId: "graphql",
+    lineComments: [/#/],
+  });
+  bluehawk.addLanguage("properties", {
+    languageId: "properties",
+    lineComments: [/#/],
+  });
+  bluehawk.addLanguage(["dockerfile", "containerfile"], {
+    languageId: "dockerfile",
+    lineComments: [/#/],
+  });
+}
