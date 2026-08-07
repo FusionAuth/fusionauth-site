044766d21e6c98cd2d9573ee41daf317d0cbaa82
Bryan Robinson <bryan.robinson@fusionauth.io>
2026-08-07 10:50:15 -0400

    Toc rework (#4457)

--- astro/src/content.config.js
+++ astro/src/content.config.js
@@ -92,6 +92,7 @@ const docsCollection = defineCollection({
     sidenavTitle: z.string().optional(),
     nestedHeadings: z.boolean().optional(),
     disableTOC: z.boolean().default(false),
+    clientToc: z.boolean().optional(),
     route: z.boolean().default(true),
     order: z.number().default(1000),
     idpDisplayName: z.string().optional(),
