8147fbc3da773c96eda1fe05d7da3f7efa352cb7
RichardJECooke <RichardJECooke@pm.me>
2026-08-12 17:58:48 +0200

    remove unused tags. fix code for breaking changes in recent FA apis

--- astro/localcode/example-testing-lambdas/app/routes/index.js
+++ astro/localcode/example-testing-lambdas/app/routes/index.js
@@ -2,26 +2,20 @@ const express = require('express');
 const router = express.Router();
 const {FusionAuthClient} = require('@fusionauth/typescript-client');
 
-// tag::clientIdSecret[]
 // set in the environment or directly
 const clientId = process.env.CLIENT_ID; // or set directly
 const clientSecret = process.env.CLIENT_SECRET; // or set directly
-// end::clientIdSecret[]
 
-// tag::baseURL[]
 const fusionAuthURL = process.env.BASE_URL;
-// end::baseURL[]
 
 const client = new FusionAuthClient('noapikeyneeded', fusionAuthURL);
 const pkceChallenge = require('pkce-challenge');
 
-// tag::logoutRoute[]
 /* logout page. */
 router.get('/logout', function (req, res, next) {
   req.session.destroy();
   res.redirect(302, '/');
 });
-// end::logoutRoute[]
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -36,7 +30,6 @@ router.get('/', function (req, res, next) {
   res.render('index', {user: req.session.user, title: 'FusionAuth Example', clientId: clientId, challenge: challenge, stateValue: stateValue, fusionAuthURL: fusionAuthURL});
 });
 
-// tag::fullOAuthCodeExchange[]
 /* OAuth return from FusionAuth */
 router.get('/oauth-redirect', function (req, res, next) {
   const stateFromServer = req.query.state;
@@ -47,30 +40,28 @@ router.get('/oauth-redirect', function (req, res, next) {
     return;
   }
 
-// tag::exchangeOAuthCode[]
   // This code stores the user in a server-side session
  client.exchangeOAuthCodeForAccessTokenUsingPKCE(req.query.code,
                                                  clientId,
                                                  clientSecret,
                                                  'http://localhost:3000/oauth-redirect',
                                                  req.session.verifier)
-// end::exchangeOAuthCode[]
       .then((response) => {
-        console.log(response.response.access_token);
-        return client.retrieveUserUsingJWT(response.response.access_token);
+        return client.retrieveUserInfoFromAccessToken(response.response.access_token);
       })
       .then((response) => {
-// tag::setUserInSession[]
-        req.session.user = response.response.user;
+        const claims = response.response;
+        req.session.user = {
+          firstName: claims.given_name || claims.email,
+          email: claims.email
+        };
         return response;
       })
-// end::setUserInSession[]
       .then((response) => {
         res.redirect(302, '/');
-      }).catch((err) => {console.log("in error"); console.error(JSON.stringify(err));});
+      }).catch((err) => {console.log("in error"); console.log(JSON.stringify(err));});
 
 });
-// end::fullOAuthCodeExchange[]
 
   // This code can be set in the last promise above to send the access and refresh tokens
   // back to the browser as secure, HTTP-only cookies, an alternative to storing user info in the session
