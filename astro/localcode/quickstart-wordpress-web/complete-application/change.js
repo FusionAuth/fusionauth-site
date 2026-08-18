e9d792e1e96d66fcf61416cf65480ec627fe0cd5
RichardJECooke <RichardJECooke@pm.me>
2026-08-18 18:06:08 +0200

    fix tests

--- astro/localcode/quickstart-wordpress-web/complete-application/change.js
+++ astro/localcode/quickstart-wordpress-web/complete-application/change.js
@@ -1,12 +1,12 @@
 function makeChange(event) {
     event.preventDefault();
     const amount = document.querySelector('#total').value;
-    const total = Math.trunc(parseFloat(amount) * 100) / 100;
-    const totalString = isNaN(total) ? '' : total.toFixed(2);
-    const nickels = Math.floor(total / 0.05);
+    const totalCents = Math.round(parseFloat(amount) * 100);
+    const totalString = isNaN(totalCents) ? '' : (totalCents / 100).toFixed(2);
+    const nickels = Math.floor(totalCents / 5);
     const nickelsString = nickels.toLocaleString();
-    const pennies = (total - 0.05 * nickels) / 0.01;
-    const penniesString = Math.ceil(Math.trunc(pennies * 100) / 100).toLocaleString();
+    const pennies = totalCents - nickels * 5;
+    const penniesString = pennies.toLocaleString();
     const hasError = !/^(\d+(\.\d*)?|\.\d+)$/.test(amount);
     let message = 'Invalid or missing amount';
     let css = 'error-message';
