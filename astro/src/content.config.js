1d8d74798f2867af68dbca5297e26930edcb6138
Nathan Contino <nathan.contino@fusionauth.io>
2026-09-10 17:01:27 -0400

    Clean up lingering brittle manual child card collections

--- astro/src/content.config.js
+++ astro/src/content.config.js
@@ -105,6 +105,8 @@ const docsCollection = defineCollection({
     cardImage: z.string().optional(),
     excludeFromNav: z.boolean().default(false),
     sectionIndex: z.boolean().default(false),
+    nextPage: z.string().optional(),
+    lastPage: z.string().optional(),
   }),
 });
 
