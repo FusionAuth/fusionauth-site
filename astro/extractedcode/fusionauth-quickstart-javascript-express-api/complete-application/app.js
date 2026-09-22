43abcc2938a04b9b5e43db6d4c3b09b62af2f775
Lewis Dwyer <lewisdwyer@gmail.com>
2026-09-22 15:31:10 +0200

    Migrate the express quickstart to extracted code

---
+++ astro/extractedcode/fusionauth-quickstart-javascript-express-api/complete-application/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// :snippet-start: verifyToken
+const verifyJWT = require('./services/verifyJWT');
+app.use(verifyJWT);
+// :snippet-end:
+
+app.use('/', indexRouter);
+
+module.exports = app;
